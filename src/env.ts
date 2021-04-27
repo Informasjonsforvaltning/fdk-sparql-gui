9f6081ebbb89596ba6f27b87d6dd8db4d48a839f
Jeff Reiffers <jeffreiffers@gmail.com>
2021-04-27 13:37:55 +0200

    chore: updated sparql backend

--- src/env.ts
+++ src/env.ts
@@ -5,7 +5,7 @@ import { Environment } from './types/enums';
 export default validateEnv(
   (window as any).env ?? {
     ENV: Environment.DEVELOPMENT,
-    SPARQL_API_HOST: 'https://www.staging.fellesdatakatalog.digdir.no/sparql',
+    SPARQL_API_HOST: 'https://sparql.staging.fellesdatakatalog.digdir.no',
     USE_DEMO_LOGO: false
   }
 );
